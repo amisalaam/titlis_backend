b3721e6f8a1593475e09ced29ffc8d5de2f99d65
amisalaam <amisalaam9@gmail.com>
2024-05-16 20:28:15 +0530

    model created

--- Credentials/models.py
+++ Credentials/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from .manager import CustomUserManager
 
 # Create your models here.
+
+class User(AbstractBaseUser,PermissionsMixin):
+    username = models.CharField(_('username'),max_length=255)
+    email = models.EmailField(_('email address'), unique=True,max_length=255)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    
+    objects = CustomUserManager()
+    
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+        
+    def __str__(self):
+        return self.email
+    
+
+    
+    
+    
+    
--- Credentials/views.py
+++ Credentials/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 
 def credentials(request):
-    pass
+    return HttpResponse('credentials')
