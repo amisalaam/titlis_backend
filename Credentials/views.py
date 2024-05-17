f26b391c6df45c77eec7591556f7ec5595dd7ba1
amisalaam <amisalaam9@gmail.com>
2024-05-17 16:28:43 +0530

    Login Completed using jwt

--- Credentials/views.py
+++ Credentials/views.py
@@ -1,8 +1,14 @@
+# views.py
+
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
 from .serializers import UserSerializer
+from .models import User
 
 class SignupView(APIView):
     permission_classes = [AllowAny]
@@ -13,3 +19,15 @@ class SignupView(APIView):
             user = serializer.save()
             return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data.update({'user': UserSerializer(self.user).data})
+        return data
+
+
+class LoginView(TokenObtainPairView):
+    permission_classes = [AllowAny]
+    serializer_class = CustomTokenObtainPairSerializer
