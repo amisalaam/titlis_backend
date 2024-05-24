45ac66a627b618feb643583b8f9fb7e504309740
amisalaam <amisalaam9@gmail.com>
2024-05-24 11:24:36 +0530

    package completed

--- Packages/admin.py
+++ Packages/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Package, TripPlans
+from .models import Package, Plans,Continents
 
 # Register your models here.
 
-admin.site.register(Package)
-admin.site.register(TripPlans)
+admin.site.register(Continents) 
+admin.site.register(Package) 
+admin.site.register(Plans)
