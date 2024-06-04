a270967768c9641767a9be42f8df730955160e84
amisalaam <amisalaam9@gmail.com>
2024-06-04 20:42:11 +0530

    Feed post created

--- Feed/views.py
+++ Feed/views.py
@@ -1,18 +1,17 @@
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import *
 from .models import Post
-# Create your views here.
-
+from .serializers import PostSerializer
 
 class PostView(APIView):
-    def get(self,request):
+    def get(self, request):
         feed = Post.objects.all()
         serializer = PostSerializer(feed, many=True)
         
-        return Response(serializer.data, status=status.HTTP_200_OK,message="Feed fetched successfully")
-        
-        
-        
+        response_data = {
+            'data': serializer.data,
+            'message': "Feed fetched successfully"
+        }
+
+        return Response(response_data, status=status.HTTP_200_OK)
