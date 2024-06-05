630de57ebdbacc6f352444f8e51a5d08097f859e
amisalaam <amisalaam9@gmail.com>
2024-06-05 22:21:00 +0530

    Travel news api view created on jun3 5

--- Feed/views.py
+++ Feed/views.py
@@ -1,8 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Post
-from .serializers import PostSerializer
+from .models import Post, TravelNews
+from .serializers import PostSerializer, TravelNewsSerializer
 
 class PostView(APIView):
     def get(self, request):
@@ -13,5 +13,18 @@ class PostView(APIView):
             'data': serializer.data,
             'message': "Feed fetched successfully"
         }
+
+        return Response(response_data, status=status.HTTP_200_OK)
+
+
+class TravelNewsView(APIView):
+    def get(self,request):
+        news = TravelNews.objects.all()
+        serilizers = TravelNewsSerializer(news, many=True)
+        
+        response_data = {
+            'data': serilizers.data,
+            'message': "News fetched successfully"
+        } 
 
         return Response(response_data, status=status.HTTP_200_OK)
